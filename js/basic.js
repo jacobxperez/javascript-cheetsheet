a5e265967ba6b9df0d22f6e0c0d32b96b1a15dd7
Jacob <jacobxperez@gmx.com>
2022-04-02 00:21:09 -0600

    Update basic.js

--- js/basic.js
+++ js/basic.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log('Task incomplete');
     }
 
+
     // else if Clause
     const size = 10;
 
@@ -63,117 +64,5 @@ document.addEventListener("DOMContentLoaded", function () {
     for (var i = 0; i < variable; i++) {
         // add code
     } // end for loop
-
-
-    // Delaying with setTimeout
-    function showMessage() {
-        console.log("delayd for 5 seconds");
-    }
-
-    var timeID = setTimeout(showMessage, 5000);
-
-
-    // Looping with setInterval
-    function drawText() {
-        console.log("looping text");
-    }
-
-    var intervalID = setInterval(drawText, 2000);
-
-    // stop time looping
-    // clearInterval(intervalID);
-
-
-    // Animating smoothly
-    function animationLoop() {
-        // animation related code
-
-        requestAnimationFrame(animationLoop);
-    }
-
-    // start animation loop
-    animationLoop();
-
-
-    // Stop requestAnimationFrame
-    var requestID;
-
-    function animationLoop() {
-        // animation related code
-
-        requestID = requestAnimationFrame(animationLoop);
-    }
-
-    cancelAnimationFrame(requestID);
-
-
-    // Function
-    function showDistance(speed, time) {
-        console.log(speed * time);
-    };
-
-    showDistance(10, 5);
-
-
-    // Function that returns data
-    function getDistance(speed, time) {
-        var distance = speed * time;
-        return distance;
-    };
-
-    var myDistance = getDistance(10, 5);
-    console.log(myDistance);
-
-
-    // Walking the DOM
-    function theDOMElementWalker() {
-        if (node.nodeType === 1) {
-            // add code
-            nodde = node.firstChild;
-
-            while (node) {
-                theDOMElementWalker(node);
-                node = node.nextSibling;
-            }
-        }
-    };
-
-    var texasRanger = document.querySelector("#texas");
-    theDOMElementWalker(texasRanger);
-
-
-    // Anonymus Function #1
-    var isLucky = function () {
-        let foo = Math.round(Math.random() * 100);
-
-        if (foo > 50) {
-            return "You are lucky!";
-
-        } else {
-            return "You are not lucky!";
-        }
-    };
-
-    let me = isLucky();
-    console.log(me);
-
-
-    // Anonymus Function #2
-    window.setTimeout(function () {
-        console.log("Everything is awsome!");
-    }, 2000);
-
-
-    // IIFE Function
-    (function () {
-        console.log();
-    })();
-
-
-    // IIFE Function with arguments
-    (function (first, last) {
-        let messege = "My name is " + first + " " + last;
-        console.log(messege);
-    })("Jacob", "Perez");
 
 });
