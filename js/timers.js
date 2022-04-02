c91f5801e57f3d226cae255e532323494acccd5d
Jacob <jacobxperez@gmx.com>
2022-04-02 00:21:16 -0600

    Create timers.js

---
+++ js/timers.js
@@ -0,0 +1,47 @@
+/* JavaScript CheetSheet
+ * <https://github.com/jacobxperez/full-screen-jquery-content-slider>
+ * Copyright (C) 2022 Jacob Perez <jacobxperez@gmx.com>
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+------------------------------------------------------------------------------*/
+
+// Delaying with setTimeout
+function showMessage() {
+    console.log("delayd for 5 seconds");
+}
+
+var timeID = setTimeout(showMessage, 5000);
+
+
+// Looping with setInterval
+function drawText() {
+    console.log("looping text");
+}
+
+var intervalID = setInterval(drawText, 2000);
+
+// stop time looping
+// clearInterval(intervalID);
+
+
+// Animating smoothly
+function animationLoop() {
+    // animation related code
+
+    requestAnimationFrame(animationLoop);
+}
+
+// start animation loop
+animationLoop();
+
+
+// Stop requestAnimationFrame
+var requestID;
+
+function animationLoop() {
+    // animation related code
+
+    requestID = requestAnimationFrame(animationLoop);
+}
+
+cancelAnimationFrame(requestID);
