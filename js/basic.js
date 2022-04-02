e96b093f2da108daf613bc71dfa07134643e3678
Jacob <jacobxperez@gmx.com>
2022-04-02 15:44:01 -0600

    Update basic.js

--- js/basic.js
+++ js/basic.js
@@ -18,7 +18,6 @@ document.addEventListener("DOMContentLoaded", function () {
     let service = 'credit card';
     let month = 'May 30th';
     let displayText = 'Your ' + service + ' bill is due on ' + month + '.';
-
     console.log(displayText);
     // Prints: Your credit card bill is due on May 30th.
 
