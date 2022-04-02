f329b948fa23ab5a79aac64a00cd78fde3b62835
Jacob <jacobxperez@gmx.com>
2022-04-02 15:38:02 -0600

    Update loops.js
    Added loops

--- js/loops.js
+++ js/loops.js
@@ -12,6 +12,13 @@ for (let i = 0; i < 4; i += 1) {
 // Output: 0, 1, 2, 3
 
 
+// Looping Through Arrays
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+// Output: Every item in the array
+
+
 // Reverse Loop
 const items = ['apricot', 'banana', 'cherry'];
 
@@ -21,6 +28,33 @@ for (let i = items.length - 1; i >= 0; i -= 1) {
 // Prints: 2. cherry
 // Prints: 1. banana
 // Prints: 0. apricot
+
+
+// Break Keyword
+for (let i = 0; i < 99; i += 1) {
+    if (i > 5) {
+        break;
+    }
+    console.log(i)
+}
+// Output: 0 1 2 3 4 5
+
+
+// Nested For Loop
+for (let outer = 0; outer < 2; outer += 1) {
+    for (let inner = 0; inner < 3; inner += 1) {
+        console.log(`${outer}-${inner}`);
+    }
+}
+/* 
+Output:
+0-0
+0-1
+0-2
+1-0
+1-1
+1-2
+*/
 
 
 // While Loop
