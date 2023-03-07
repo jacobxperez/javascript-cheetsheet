8c568d822d03185676dd1224524c7bb8d2434487
Jacob <jacobxperez@gmx.com>
2023-03-07 12:22:57 -0700

    Create toggle.js

---
+++ js/modules/toggle.js
@@ -0,0 +1,31 @@
+const toggle = () => {
+    const getToggle = document.querySelectorAll('[data-toggle]')
+
+    // toggles attribute
+    function toggleAttr(item) {
+        item.addEventListener('click', (e) => {
+            item.hasAttribute('data-state', 'active')
+                ? item.removeAttribute('data-state')
+                : item.setAttribute('data-state', 'active')
+            e.stopPropagation()
+        })
+    }
+
+    getToggle.forEach(toggleAttr)
+
+    // remove data-state active
+    function removeAtt(item, e) {
+        if (
+            (e.target !== item && item.matches('[data-toggle~="pop"]')) ||
+            item.matches('[data-toggle~="tooltip"]')
+        ) {
+            item.removeAttribute('data-state')
+        }
+    }
+
+    document.addEventListener('click', (e) => {
+        getToggle.forEach((item) => removeAtt(item, e))
+    })
+}
+
+export {toggle}
